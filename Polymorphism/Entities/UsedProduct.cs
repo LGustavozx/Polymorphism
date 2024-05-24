64c6fce390ff7516d63e1782d6f5e9a77607ceef
Gustavo <gustavoskt.8@hotmail.com>
2024-05-23 23:14:40 -0300

    exercicio resolvido sobre Polymorphism

---
+++ Polymorphism/Entities/UsedProduct.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace Polymorphism.Entities
+{
+    class UsedProduct : Product
+    {
+        public DateTime ManufactureDate { get; set; }
+
+        public UsedProduct()
+        {
+        }
+
+        public UsedProduct(string name, double price, DateTime manufactureDate) : base(name, price)
+        {
+            ManufactureDate = manufactureDate;
+        }
+
+        public override string PriceTag()
+        {
+            return $"{Name} (used) $ {Price:F2} (Manufacture date: {ManufactureDate.ToString("dd/MM/yyyy")})";
+        }
+    }
+}
+
+    
